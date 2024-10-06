bf3769b75ec2f57603e193ad7c78cfd217c76a37
/\/\@SoOd <42731850+masoodMEH@users.noreply.github.com>
2024-10-06 21:55:09 +0330

    implementing button component

---
+++ src/app/_components/button/button.tsx
@@ -0,0 +1,53 @@
+import classNames from "classnames";
+import React from "react";
+
+import { Size } from "../types/size.type";
+import { ButtonProps, ButtonShape } from "./button.types";
+
+const sizeClasses: Record<Size, string> = {
+  tiny: "btn-xs",
+  small: "btn-sm",
+  normal: "",
+  large: "btn-lg",
+};
+
+const shapeClasses: Record<ButtonShape, string> = {
+  wide: "btn-wide",
+  full: "btn-full",
+  square: "btn-square",
+  default: "",
+};
+
+export const Button: React.FC<ButtonProps> = ({
+  variant,
+  size = "normal",
+  isDisabled = false,
+  isOutline = false,
+  shape = "default",
+  isLoading = false,
+  loadingType = "spinner",
+  loadingText = "در حال ارسال درخواست...",
+  type = "button",
+  isLink = false,
+  animatedIcon = false,
+  children,
+  className,
+  ...rest
+}: ButtonProps) => {
+  const classes = classNames(
+    "btn",
+    className,
+    { "btn-outline": isOutline },
+    { btnlink: isLink },
+    { "animated-icon": animatedIcon },
+    { "pointer-events-none opacity-80": isLoading },
+    { [`btn-${variant}`]: variant },
+    { [`${sizeClasses}`]: size },
+    { [`${shapeClasses[shape]}`]: shape }
+  );
+  return (
+    <button type={type} disabled={isDisabled} {...rest} className={classes}>
+      {isLoading ? loadingText : children}
+    </button>
+  );
+};
