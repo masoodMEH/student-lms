f320d7db9f3eef769f14f63fe7e7350602496290
/\/\@SoOd <42731850+masoodMEH@users.noreply.github.com>
2025-01-09 16:35:22 +0330

    36- fetching newest blog posts

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,11 @@
 import { homeFeatures } from "@/data/home-features";
+import { BlogPostSummary } from "@/types/blog-post-summary.interface";
 import { CourseSummary } from "@/types/course-summary.interface";
 import { CourseCardList } from "./(courses)/courses/_components/course-card-list";
+import { Button } from "./_components/button";
 import Feature from "./_components/feature/feature";
 import { HomeHeroSection } from "./_components/home-hero-section/home-hero-section";
+import { IconArrowLeftFill } from "./_components/icons/icons";
 
 async function getNewestCourses(count: number): Promise<CourseSummary[]> {
   const res = await fetch(
@@ -16,9 +19,24 @@ async function getNewestCourses(count: number): Promise<CourseSummary[]> {
   return res.json();
 }
 
+async function getNewestPosts(count: number): Promise<BlogPostSummary[]> {
+  const res = await fetch(`https://api.classbon.com/api/blog/newest/${count}`, {
+    next: {
+      revalidate: 24 * 60 * 60, //86400 seconds - 1 day,
+    },
+  });
+  return res.json();
+}
+
 export default async function Home() {
-  const newestCourses = await getNewestCourses(4);
-  console.log(newestCourses[0].title);
+  const newestCoursesData = getNewestCourses(4);
+  const newestBlogPostsData = getNewestPosts(4);
+
+  const [newestCourses, newestBlogPosts] = await Promise.all([
+    newestCoursesData,
+    newestBlogPostsData,
+  ]);
+
   return (
     <>
       <HomeHeroSection />
@@ -35,6 +53,45 @@ export default async function Home() {
           <p>برای به روز ماندن ، یاد گرفتن نکنه های تازه ضروریه!</p>
         </div>
         <CourseCardList courses={newestCourses} />
+      </section>
+
+      <section className="px-2 my-40">
+        {/* <div className="sticky top-0 pt-0 text-center"> */}
+        <div className="relative pt-0 text-center">
+          <div className="bg-primary pointer-events-none absolute left-1/2 aspect-square w-1/2 -translate-x-1/2 -top-96 rounded-full opacity-10 blur-3xl"></div>
+
+          <h2
+            lang="en"
+            className="gradient leading-[1.3] relative z-10 mx-auto inline-block text-[clamp(2rem,6vw,5.5rem)] font-black"
+          >
+            ReactJs & Next.js
+          </h2>
+          <p className="text-base-content/70  relative z-[2] py-4 m-auto md:text-3xl max-w-5xl font-light !leading-[1.7]">
+            ری‌اکت و نکست‌جی‌اس برترین کتابخونه‌های فرانت‌اند و یکه‌تاز دنیای
+            وب! پیشرفته‌ترین مباحث رو اینجا می تونی پیدا کنی. پس همین الان
+            یادگیری رو شروع کن ما هم از ابتدای مسیر با آموزش‌های تخصصی و کاملاً
+            کاربردی کنارت هستیم.
+          </p>
+          <div className="flex flex-col lg:flex-row items-center gap-3 justify-center">
+            <Button
+              variant="primary"
+              size="large"
+              className="mt-7"
+              animatedIcon={true}
+            >
+              دوره‌های ری اکت و نکست‌ جی‌اس
+              <IconArrowLeftFill fill="currentColor" />
+            </Button>
+            <Button
+              variant="neutral"
+              size="large"
+              className="mt-7"
+              animatedIcon={true}
+            >
+              مقالات ری اکت و نکست‌ جی‌اس
+            </Button>
+          </div>
+        </div>
       </section>
     </>
   );
---
+++ src/types/blog-post-summary.interface.ts
@@ -0,0 +1,11 @@
+export interface BlogPostSummary {
+  title: string;
+  slug: string;
+  postData: string;
+  thumbnailURL: string;
+  studyTime: number;
+  numberOfViews: number;
+  author: string;
+  numberOfComments: number;
+  isNew: boolean;
+}
