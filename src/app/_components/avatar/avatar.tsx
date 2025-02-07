b07c15bc5602cf2ed789b8a257e82993bd4b3ab0
/\/\@SoOd <42731850+masoodMEH@users.noreply.github.com>
2025-02-07 20:29:26 +0330

    43 - implementing avatar component

---
+++ src/app/_components/avatar/avatar.tsx
@@ -0,0 +1,31 @@
+import classNames from 'classnames'
+import Image from 'next/image'
+import React from 'react'
+import { IconUserProfile } from '../icons/icons'
+import { Size } from '../types/size.type'
+import { AvatarProps } from './avatar.types'
+
+const sizeClasses: Record<Size, number> = {
+    tiny: 40,
+    small: 50,
+    normal: 70,
+    large: 120
+}
+
+export const Avatar: React.FC<AvatarProps> = ({ variant = "primary", className, size = "normal", src, alt = "" }) => {
+    const classes = classNames('avatar', className, {
+        [`avatar-${variant}`]: variant,
+        [`${sizeClasses[size]}`]: size,
+    })
+    return (
+        <div className={classes} style={{ width: sizeClasses[size], height: sizeClasses[size] }}>
+            {
+                src ? (
+                    <Image src={src} width={sizeClasses[size]} height={sizeClasses[size]} alt={alt} />
+                ) : (
+                    <IconUserProfile width={sizeClasses[size] / 2} height={sizeClasses[size] / 2} />
+                )
+            }
+        </div>
+    )
+}
