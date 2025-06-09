e15df956ecb15a7cfc68cb160cb291d4ce823a74
MaSoOd <42731850+masoodMEH@users.noreply.github.com>
2025-06-09 20:23:35 +0330

    adding something

--- src/app/(blog)/blog/page.tsx
+++ src/app/(blog)/blog/page.tsx
@@ -1,5 +1,5 @@
 export default async function Blog() {
   return (
-    <div className="text-5xl flex justify-center items-center">Blog Page</div>
+    <div className="text-5xl flex justify-center items-center">Blogg Page</div>
   );
 }
